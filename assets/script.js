a2a6b222ca36ceb6938a63c0c0840d3f46a8eada
George Sandoval <gssandoval09@gmail.com>
2021-11-06 21:34:41 -0500

    fixed quiz,timer works,score is produced at the end, questions work

--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,109 @@
 // create var for current question index variable  " equal to 0"
-var timer= 40
-var timerElement= document.getElementById("timer")
-
-var startButton= document.getElementById("startButton")
+var timer = 0;
+var startButton = document.getElementById("answerButton");
 //  use equal to 0 with increase by 1 increment to move on to next question 
-var currentIndex= 0
+var score = 0;
+var currentIndex = 0;
+var interval = "";
 // create var that has array of objects, inside objects include key value pairs (questions to ask)
-var questionBox= [
+var questionList = [
     {
-        question: "what is iron mans real name", 
+        question: "what is iron mans real name",
         choices: [
-            "jerome","manuel","tony stark","donald",
+            "jerome", "manuel", "tony stark", "donald",
 
         ],
         answer: "tony stark",
     },
     {
-        question: "what is Captain America's shield made of?", 
+        question: "what is Captain America's shield made of?",
         choices: [
-            "vibranium", "adamantium", "norse steel", ""
+            "vibranium", "adamantium", "norse steel", "wood"
         ],
         answer: "vibranium",
     },
     {
-        question: "what power does professor x have?", 
+        question: "what power does professor x have?",
         choices: [
-                "flight", "mind control", "super strength", "cellular reproduction"
+            "flight", "mind control", "super strength", "cellular reproduction"
         ],
         answer: "mind control"
     },
-]
+];
 // create choices (a.b,c,d)
 
 // include answers 
 
 
 // function must be made to start the quiz/ also start the countdown at same time. 
-function startQuiz(){
-    
-    var countdown= setInterval(function(){
-        timer-- 
-        timerElement.textContent= timer +
-        "seconds left"
-        if   (timer===0){
-        
-        clearInterval(countdown)
-
-    }
-    }, 1000) 
-    }
-    // another function that gets the questions we want to ask from variable we created on line 3
-function displayQuestions(){
-    //target our question by current index number, with array of objects, be able to loop through all objects in our array and isplay one question at a time, using index number! dynamically create buttons for each choice. display question function wil be inside of our start quiz function. 
-    
+function startTimer(duration) {
+    timer = duration;
+    var minutes = 0;
+    var seconds = 0;
+    var timerElement = document.getElementById("timer");
+    interval = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = parseInt(timer % 60, 10);
+        seconds =  seconds < 10 ? "0" + seconds : seconds;
+        timerElement.textContent = `${minutes}:${seconds}`;
+        if (timer > 0) {
+            timer--
+        }
+    }, 1000);
 }
+
+// another function that gets the questions we want to ask from variable we created on line 3
+function showQuestions() {
+    var currentQuestion = questionList[currentIndex];
+    console.log("reached questions");
+    var temp = `
+    <container>
+    <div id="question"> Question#${currentIndex} </div>
+
+    <div id="question">${currentQuestion.question}</div>
+    <div id="answer-buttons" class="btn-grid">
+      <button onclick="answerQuestion('${currentQuestion.choices[0]}')" id="answerButton" class="btn">${currentQuestion.choices[0]}</button>
+      <button onclick="answerQuestion('${currentQuestion.choices[1]}')" id="answerButton" class="btn">${currentQuestion.choices[1]}</button>
+      <button onclick="answerQuestion('${currentQuestion.choices[2]}')" id="answerButton" class= "btn">${currentQuestion.choices[2]}</button>
+      <button onclick="answerQuestion('${currentQuestion.choices[3]}')" id="answerButton" class="btn">${currentQuestion.choices[3]}</button>
+    </div>
+    </container>`;
+
+    document.getElementById("questionContainer").innerHTML = temp;
+}
+function answerQuestion(answer) {
+    var currentQuestion = questionList[currentIndex];
+    var isCorrect =(answer == currentQuestion.answer);
+    if (isCorrect) {
+        score++;
+    }  
+    else {
+        timer-= 30;
+    }
+   
+    currentIndex++;
+    if (currentIndex>= questionList.length) {
+        document.getElementById('questionContainer').innerHTML = `${score}/${questionList.length}`;
+        clearInterval(interval);
+    }
+    else {
+        document.getElementById('questionContainer').innerHTML = `
+        <div> That answer was ${isCorrect} </div>
+        <button id="answerButton" onclick="showQuestions()" class="btn">Next Question</button>
+        `;
+    }
+}
+
+function startQuiz() {
+    console.log('starting...');
+    startTimer(60 * 5);
+    showQuestions();
+}
+
 
 // new function made to read if answer is wrong, if answer wrong, less time for quiz, will display the next question once answered
 
 // another function showing end highscore if all questions answered and/or if timer ends
-startButton.addEventListener("click", startQuiz)
+startButton.addEventListener("click", startQuiz);
 
